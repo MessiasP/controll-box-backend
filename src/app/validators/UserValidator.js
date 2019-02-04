93dbe5facdddb791e0649791bb32179d393aa001
Messias <messiasp_souza@outlook.com>
2019-02-04 10:00:15 -0200

    Headear novo

--- src/app/validators/UserValidator.js
+++ src/app/validators/UserValidator.js
@@ -3,6 +3,6 @@ const Joi = require('joi')
 module.exports = {
   body: {
     login: Joi.string().required().min(5),
-    password: Joi.string().required().min(5)
+    password: Joi.string().required().min(6)
   }
 }
