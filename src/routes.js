96aedce83db03869afb972a56e71dad1f560008d
Messias <messiasp_souza@outlook.com>
2019-02-09 23:54:03 -0200

    Antes do JWT

--- src/routes.js
+++ src/routes.js
@@ -19,7 +19,7 @@ routes.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
@@ -28,7 +28,7 @@ routes.use((req, res, next) => {
 
 
     // res.header("Access-Control-Allow-Origin", "*")
-    // res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers');
     next()
 })
 
